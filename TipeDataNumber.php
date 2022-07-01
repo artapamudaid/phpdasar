347c75e2ee65c70a48fa8f4774924e45ecdeb050
artapamudaid <pamuda.id@gmail.com>
2022-07-01 13:40:40 +0700

    tipe data

---
+++ TipeDataNumber.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "Decimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(0123);
+
+echo "Hexadecimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111111);
+
+echo "Underscore in number : ";
+var_dump(1_234_234_234);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point e Notaion Plus 1.2 x 1000: ";
+var_dump(1.2e3);
+
+echo "Floating Point e Notaion Minus 7 x 0.001: ";
+var_dump(7e-3);
+
+echo "Underscore in float : ";
+var_dump(1_234.123);
---
+++ TipeDataString.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 'Nama : ';
+echo 'Arta Tri Pamuda';
+echo "\n";
+
+echo "Nama : ";
+echo "Arta\t Tri\t Pamuda\n";
+
+echo <<<ARTA
+Selamat Belajar PHP
+Ini saya Arta sedang belajar
+PHP Dasar Programmer Zaman Now
+
+ARTA;
+
+echo <<<'ARTA'
+Selamat Belajar PHP
+Ini saya Arta sedang belajar
+PHP Dasar Programmer Zaman Now
+
+ARTA;
